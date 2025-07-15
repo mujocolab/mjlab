e48da541e2e0489209a5b947dfdaced029d48836
Kevin Zakka <kevinarmandzakka@gmail.com>
2025-07-15 01:33:15 -0400

    Directly operate on WarpBridge view in observations and rewards.

--- mjlab/envs/mdp/observations.py
+++ mjlab/envs/mdp/observations.py
@@ -1,7 +1,6 @@
 """Useful methods for MDP observations."""
 
 import torch
-import warp as wp
 
 from mjlab.envs.manager_based_rl_env import ManagerBasedRLEnv
 from mjlab.managers.scene_entity_config import SceneEntityCfg
@@ -29,5 +28,4 @@ def joint_pos(
   env: ManagerBasedRLEnv,
   entity_cfg: SceneEntityCfg = SceneEntityCfg("robot"),
 ) -> torch.Tensor:
-  qpos_t = wp.to_torch(env.sim.wp_data.qpos)
-  return qpos_t[:, entity_cfg.qpos_ids]
+  return env.sim.data.qpos[:, entity_cfg.qpos_ids]
--- mjlab/envs/mdp/rewards.py
+++ mjlab/envs/mdp/rewards.py
@@ -1,41 +1,14 @@
 """Useful methods for MPD rewards."""
 
 import torch
-import warp as wp
 
 from mjlab.managers.scene_entity_config import SceneEntityCfg
 from mjlab.envs.manager_based_rl_env import ManagerBasedRLEnv
-
-"""
-joint_torques_l2(
-  env=env,
-  # Penalize all joints.
-  asset_cfg=AssetCfg(joint_names=[".*"]),
-)
-
-joint_torques_l2(
-  env=env,
-  # Penalize all joints whose name starts with g1/.
-  # Note the scene attachment process prefixes entities
-  # with a name so for example if I attach a g1 to
-  # the scene and specify its name as g1, then all
-  # elements of the g1 such as its joints will start
-  # with `g1/`.
-  asset_cfg=AssetCfg(joint_names=[r"^g1/.*"])
-)
-
-# They might not all have names but I can specify it from
-# an entity.
-joint_torques_l2(
-  env=env,
-  asset_cfg=AssetCfg(name="g1")
-)
-"""
 
 
 def joint_torques_l2(
   env: ManagerBasedRLEnv,
   entity_cfg: SceneEntityCfg = SceneEntityCfg("robot"),
 ) -> torch.Tensor:
-  torques = wp.to_torch(env.sim.wp_data.qfrc_actuator)[:, entity_cfg.dof_ids]
+  torques = env.sim.data.qfrc_actuator[:, entity_cfg.dof_ids]
   return torch.sum(torch.square(torques), dim=-1)
