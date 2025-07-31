1fe9d3c81c1f7196a01a07cb3eb4d88feb4cfece
Kevin Zakka <kevinarmandzakka@gmail.com>
2025-07-30 20:35:01 -0400

    Make RNG seeding function match issacsim.

--- mjlab/utils/random.py
+++ mjlab/utils/random.py
@@ -6,16 +6,17 @@ import warp as wp
 
 
 def seed_rng(seed: int, torch_deterministic: bool = False) -> None:
-  np.random.seed(seed)
   random.seed(seed)
+  np.random.seed(seed)
+  torch.manual_seed(seed)
   os.environ["PYTHONHASHSEED"] = str(seed)
-
+  torch.cuda.manual_seed(seed)
+  torch.cuda.manual_seed_all(seed)
   wp.rand_init(seed)
 
-  torch.manual_seed(seed)
-  torch.cuda.manual_seed(seed)
-
   if torch_deterministic:
+    # refer to https://docs.nvidia.com/cuda/cublas/index.html#cublasApi_reproducibility
+    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
     torch.use_deterministic_algorithms(True)
