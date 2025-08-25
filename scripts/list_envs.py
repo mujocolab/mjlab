b666a8c88974ad15a300535bd865c7b48c0994b0
louislelay <louislelay@pal-robotics.com>
2025-08-25 12:54:18 +0200

    formats

--- scripts/list_envs.py
+++ scripts/list_envs.py
@@ -1,36 +1,37 @@
 """Script to list MJLab environments."""
-import mjlab.tasks
 
 import gymnasium as gym
 from prettytable import PrettyTable
 
+
 def main():
-    """Print all environments registered whose id contains `prefix_substring`."""
-    prefix_substring = "Mjlab-"
+  """Print all environments registered whose id contains `Mjlab-`."""
+  prefix_substring = "Mjlab-"
 
-    table = PrettyTable(["#", "Task ID", "Entry Point", "env_cfg_entry_point"])
-    table.title = "Available Environments in Isaac Lab (filtered)"
-    table.align["Task ID"] = "l"
-    table.align["Entry Point"] = "l"
-    table.align["env_cfg_entry_point"] = "l"
+  table = PrettyTable(["#", "Task ID", "Entry Point", "env_cfg_entry_point"])
+  table.title = "Available Environments in Mjlab"
+  table.align["Task ID"] = "l"
+  table.align["Entry Point"] = "l"
+  table.align["env_cfg_entry_point"] = "l"
 
-    idx = 0
-    for spec in gym.registry.values():
-        try:
-            if prefix_substring in spec.id:
-                env_cfg_ep = spec.kwargs.get("env_cfg_entry_point", "")
-                table.add_row([idx + 1, spec.id, spec.entry_point, env_cfg_ep])
-                idx += 1
-        except Exception:
-            continue
+  idx = 0
+  for spec in gym.registry.values():
+    try:
+      if prefix_substring in spec.id:
+        env_cfg_ep = spec.kwargs.get("env_cfg_entry_point", "")
+        table.add_row([idx + 1, spec.id, spec.entry_point, env_cfg_ep])
+        idx += 1
+    except Exception:
+      continue
 
-    print(table)
-    if idx == 0:
-        print(f"[INFO] No tasks matched filter: '{prefix_substring}'")
-    return idx
+  print(table)
+  if idx == 0:
+    print(f"[INFO] No tasks matched filter: '{prefix_substring}'")
+  return idx
+
 
 if __name__ == "__main__":
-    try:
-        main()
-    except Exception as e:
-        raise RuntimeError(f"Error listing environments: {e}")
+  try:
+    main()
+  except Exception as e:
+    raise RuntimeError(f"Error listing environments: {e}") from e
