6abc771ce32ed076432640bc1796529ddd8cff99
Kevin Zakka <kevinarmandzakka@gmail.com>
2025-09-22 11:53:20 -0700

    Cleanup tracking task.

--- src/mjlab/tasks/tracking/tracking_env_cfg.py
+++ src/mjlab/tasks/tracking/tracking_env_cfg.py
@@ -40,7 +40,7 @@ SCENE_CFG = SceneCfg(
 VIEWER_CONFIG = ViewerConfig(
   origin_type=ViewerConfig.OriginType.ASSET_BODY,
   asset_name="robot",
-  body_name="torso_link",
+  body_name="",  # Override in robot specific cfg.
   distance=3.0,
   elevation=-5.0,
   azimuth=90.0,
@@ -64,7 +64,7 @@ class CommandsCfg:
     },
     velocity_range=VELOCITY_RANGE,
     joint_position_range=(-0.1, 0.1),
-    # Placeholders.
+    # Override in robot specific cfg.
     motion_file="",
     reference_body="",
     body_names=[],
@@ -159,7 +159,7 @@ class EventCfg:
     mode="startup",
     func=mdp.randomize_field,
     params={
-      "asset_cfg": SceneEntityCfg("robot", body_names="torso_link"),
+      "asset_cfg": SceneEntityCfg("robot", body_names=[]),  # Override in robot cfg.
       "operation": "add",
       "field": "body_ipos",
       "ranges": {
@@ -185,9 +185,7 @@ class EventCfg:
     mode="startup",
     func=mdp.randomize_field,
     params={
-      "asset_cfg": SceneEntityCfg(
-        "robot", geom_names=[r"^(left|right)_foot[1-7]_collision$"]
-      ),
+      "asset_cfg": SceneEntityCfg("robot", geom_names=[]),  # Override in robot cfg.
       "operation": "abs",
       "field": "geom_friction",
       "ranges": (0.3, 1.2),
@@ -279,19 +277,9 @@ class TerminationsCfg:
     params={
       "command_name": "motion",
       "threshold": 0.25,
-      "body_names": [
-        "left_ankle_roll_link",
-        "right_ankle_roll_link",
-        "left_wrist_yaw_link",
-        "right_wrist_yaw_link",
-      ],
+      "body_names": [],  # Override in robot cfg.
     },
   )
-
-
-@dataclass
-class CurriculumCfg:
-  pass
 
 
 SIM_CFG = SimulationCfg(
@@ -314,8 +302,7 @@ class TrackingEnvCfg(ManagerBasedRlEnvCfg):
   rewards: RewardCfg = field(default_factory=RewardCfg)
   terminations: TerminationsCfg = field(default_factory=TerminationsCfg)
   events: EventCfg = field(default_factory=EventCfg)
-  curriculum: CurriculumCfg = field(default_factory=CurriculumCfg)
   sim: SimulationCfg = field(default_factory=lambda: SIM_CFG)
   viewer: ViewerConfig = field(default_factory=lambda: VIEWER_CONFIG)
-  decimation: int = 4
+  decimation: int = 4  # 50 Hz control frequency.
   episode_length_s: float = 10.0
