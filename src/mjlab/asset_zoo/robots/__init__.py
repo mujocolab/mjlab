63a78d84ea81620a8e35d22f09de8c57529deec5
Kevin Zakka <kevinarmandzakka@gmail.com>
2025-09-24 17:59:13 -0700

    Remove T1.

--- src/mjlab/asset_zoo/robots/__init__.py
+++ src/mjlab/asset_zoo/robots/__init__.py
@@ -1,9 +1,7 @@
-from mjlab.asset_zoo.robots.booster_t1.t1_constants import T1_ROBOT_CFG
 from mjlab.asset_zoo.robots.unitree_g1.g1_constants import G1_ROBOT_CFG
 from mjlab.asset_zoo.robots.unitree_go1.go1_constants import GO1_ROBOT_CFG
 
 __all__ = (
-  "T1_ROBOT_CFG",
   "G1_ROBOT_CFG",
   "GO1_ROBOT_CFG",
 )
