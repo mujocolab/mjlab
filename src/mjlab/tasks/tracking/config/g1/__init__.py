7ab73b039c3e6709689ec2ab14f6c0f2639517b4
louislelay <louislelay@pal-robotics.com>
2025-09-26 12:35:55 +0200

    unifies tracking id to velocity id

--- src/mjlab/tasks/tracking/config/g1/__init__.py
+++ src/mjlab/tasks/tracking/config/g1/__init__.py
@@ -1,7 +1,7 @@
 import gymnasium as gym
 
 gym.register(
-  id="Mjlab-Tracking-Flat-G1",
+  id="Mjlab-Tracking-Flat-Unitree-G1",
   entry_point="mjlab.envs:ManagerBasedRlEnv",
   disable_env_checker=True,
   kwargs={
@@ -11,7 +11,7 @@ gym.register(
 )
 
 gym.register(
-  id="Mjlab-Tracking-Flat-G1-Play",
+  id="Mjlab-Tracking-Flat-Unitree-G1-Play",
   entry_point="mjlab.envs:ManagerBasedRlEnv",
   disable_env_checker=True,
   kwargs={
