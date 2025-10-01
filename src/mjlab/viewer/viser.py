0cc4de3a9a8b7a35d3cf84a1f2379e3b538fbabb
Kevin Zalla <kevinarmandzakka@gmail.com>
2025-09-30 21:29:03 -0700

    Add Apache 2.0 license headers to Python files in `src/mjlab`.
    I used `addlicense` with the following command:
    
    ```bash
    find src/mjlab -type f -name "*.py" \
        ! -path "*/third_party/*" \
        ! -path "*/typings/*" \
        -exec addlicense -c "The MjLab Developers" -l apache -v {} +
    ```

--- src/mjlab/viewer/viser.py
+++ src/mjlab/viewer/viser.py
@@ -1,3 +1,17 @@
+# Copyright 2025 The MjLab Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Mjlab viewer based on Viser.
 
 Adapted from an MJX visualizer by Chung Min Kim: https://github.com/chungmin99/
