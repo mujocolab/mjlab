6a66ee58f47c2a5f7e068fee0c5cc41d1d5c440d
Kevin Zakka <kevinarmandzakka@gmail.com>
2025-10-20 12:03:12 -0700

    Increase njmax for flaky test.

--- tests/test_builtin_sensor.py
+++ tests/test_builtin_sensor.py
@@ -70,7 +70,7 @@ def test_jointpos_sensor(articulated_robot_xml, device):
 
   scene = Scene(scene_cfg, device)
   model = scene.compile()
-  sim_cfg = SimulationCfg()
+  sim_cfg = SimulationCfg(njmax=16)
   sim = Simulation(num_envs=2, cfg=sim_cfg, model=model, device=device)
   scene.initialize(sim.mj_model, sim.model, sim.data)
 
@@ -111,7 +111,7 @@ def test_accelerometer_sensor(articulated_robot_xml, device):
 
   scene = Scene(scene_cfg, device)
   model = scene.compile()
-  sim_cfg = SimulationCfg()
+  sim_cfg = SimulationCfg(njmax=16)
   sim = Simulation(num_envs=2, cfg=sim_cfg, model=model, device=device)
   scene.initialize(sim.mj_model, sim.model, sim.data)
 
@@ -169,7 +169,7 @@ def test_multiple_sensors(articulated_robot_xml, device):
 
   scene = Scene(scene_cfg, device)
   model = scene.compile()
-  sim_cfg = SimulationCfg()
+  sim_cfg = SimulationCfg(njmax=16)
   sim = Simulation(num_envs=1, cfg=sim_cfg, model=model, device=device)
   scene.initialize(sim.mj_model, sim.model, sim.data)
 
