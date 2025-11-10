4d78f8fabb6d86ba36a7a88cf696d8c6959702f7
louislelay <louislelay@pal-robotics.com>
2025-11-10 10:51:02 +0100

    modifies entry point for env configs

--- src/mjlab/tasks/tracking/config/g1/__init__.py
+++ src/mjlab/tasks/tracking/config/g1/__init__.py
@@ -1,17 +1,12 @@
 import gymnasium as gym
 
-from .env_cfgs import (
-  G1_FLAT_TRACKING_ENV_CFG,
-  G1_FLAT_TRACKING_NO_STATE_ESTIMATION_ENV_CFG,
-)
-
 gym.register(
   id="Mjlab-Tracking-Flat-Unitree-G1",
   entry_point="mjlab.envs:ManagerBasedRlEnv",
   disable_env_checker=True,
   kwargs={
-    "env_cfg_entry_point": G1_FLAT_TRACKING_ENV_CFG,
-    "rl_cfg_entry_point": f"{__name__}.rl_cfg:G1FlatPPORunnerCfg",
+    "env_cfg_entry_point": f"{__name__}.env_cfgs:UNITREE_G1_FLAT_TRACKING_ENV_CFG",
+    "rl_cfg_entry_point": f"{__name__}.rl_cfg:UnitreeG1FlatPPORunnerCfg",
   },
 )
 
@@ -21,7 +16,7 @@ gym.register(
   entry_point="mjlab.envs:ManagerBasedRlEnv",
   disable_env_checker=True,
   kwargs={
-    "env_cfg_entry_point": G1_FLAT_TRACKING_NO_STATE_ESTIMATION_ENV_CFG,
-    "rl_cfg_entry_point": f"{__name__}.rl_cfg:G1FlatPPORunnerCfg",
+    "env_cfg_entry_point": f"{__name__}.env_cfgs:UNITREE_G1_FLAT_TRACKING_NO_STATE_ESTIMATION_ENV_CFG",
+    "rl_cfg_entry_point": f"{__name__}.rl_cfg:UnitreeG1FlatPPORunnerCfg",
   },
 )
--- src/mjlab/tasks/velocity/config/g1/__init__.py
+++ src/mjlab/tasks/velocity/config/g1/__init__.py
@@ -1,13 +1,11 @@
 import gymnasium as gym
 
-from .env_cfgs import UNITREE_G1_FLAT_ENV_CFG, UNITREE_G1_ROUGH_ENV_CFG
-
 gym.register(
   id="Mjlab-Velocity-Rough-Unitree-G1",
   entry_point="mjlab.envs:ManagerBasedRlEnv",
   disable_env_checker=True,
   kwargs={
-    "env_cfg_entry_point": UNITREE_G1_ROUGH_ENV_CFG,
+    "env_cfg_entry_point": f"{__name__}.env_cfgs:UNITREE_G1_ROUGH_ENV_CFG",
     "rl_cfg_entry_point": f"{__name__}.rl_cfg:UnitreeG1PPORunnerCfg",
   },
 )
@@ -17,7 +15,7 @@ gym.register(
   entry_point="mjlab.envs:ManagerBasedRlEnv",
   disable_env_checker=True,
   kwargs={
-    "env_cfg_entry_point": UNITREE_G1_FLAT_ENV_CFG,
+    "env_cfg_entry_point": f"{__name__}.env_cfgs:UNITREE_G1_FLAT_ENV_CFG",
     "rl_cfg_entry_point": f"{__name__}.rl_cfg:UnitreeG1PPORunnerCfg",
   },
 )
