1462916c4c0248b1169c11caa5fb86b8c40a78d3
louislelay <louislelay@pal-robotics.com>
2026-01-12 14:36:53 +0100

    fixes collision issue

--- src/mjlab/asset_zoo/robots/unitree_go1/go1_constants.py
+++ src/mjlab/asset_zoo/robots/unitree_go1/go1_constants.py
@@ -124,7 +124,7 @@ FULL_COLLISION = CollisionCfg(
   friction={_foot_regex: (0.6,)},
   solimp={_foot_regex: (0.9, 0.95, 0.023)},
   contype=1,
-  conaffinity=0,
+  conaffinity={_foot_regex: 1, ".*_collision": 0},
 )
 
 ##
