0a4c524ffc2d86f4e83a7fc2f13a4539f4d98260
Louis Le Lay <louislelay@pal-robotics.com>
2026-01-27 18:11:49 +0100

    adds the dict possibility for the bias_ranges

--- src/mjlab/envs/mdp/events.py
+++ src/mjlab/envs/mdp/events.py
@@ -831,7 +831,7 @@ def randomize_effort_limits(
 def randomize_encoder_bias(
   env: ManagerBasedRlEnv,
   env_ids: torch.Tensor | None,
-  bias_range: Tuple[float, float],
+  bias_range: Tuple[float, float] | Dict[str, Tuple[float, float]],
   asset_cfg: SceneEntityCfg = _DEFAULT_ASSET_CFG,
 ) -> None:
   """Randomize encoder bias to simulate joint encoder calibration errors.
@@ -845,26 +845,42 @@ def randomize_encoder_bias(
   else:
     env_ids = env_ids.to(env.device, dtype=torch.int)
 
-  joint_ids = asset_cfg.joint_ids
-  if isinstance(joint_ids, slice):
-    num_joints = asset.num_joints
-    joint_ids_tensor = torch.arange(num_joints, device=env.device)
+  if isinstance(bias_range, dict):
+    for joint_name, (min_val, max_val) in bias_range.items():
+      joint_ids, _ = asset.find_joints(joint_name)
+      joint_ids_tensor = torch.tensor(joint_ids, device=env.device)
+      
+      bias_samples = sample_uniform(
+        torch.tensor(min_val, device=env.device),
+        torch.tensor(max_val, device=env.device),
+        (len(env_ids), len(joint_ids)),
+        env.device,
+      )
+
+      asset.data.encoder_bias[env_ids[:, None], joint_ids_tensor] = bias_samples
+
   else:
-    joint_ids_tensor = torch.tensor(joint_ids, device=env.device)
+    global_bias_range = cast(Tuple[float, float], bias_range)
 
-  num_joints = len(joint_ids_tensor)
-  bias_samples = sample_uniform(
-    torch.tensor(bias_range[0], device=env.device),
-    torch.tensor(bias_range[1], device=env.device),
-    (len(env_ids), num_joints),
-    env.device,
-  )
+    joint_ids = asset_cfg.joint_ids
+    if isinstance(joint_ids, slice):
+      num_joints = asset.num_joints
+      joint_ids_tensor = torch.arange(num_joints, device=env.device)
+    else:
+      joint_ids_tensor = torch.tensor(joint_ids, device=env.device)
 
-  if isinstance(joint_ids, slice):
-    asset.data.encoder_bias[env_ids] = bias_samples
-  else:
-    asset.data.encoder_bias[env_ids[:, None], joint_ids_tensor] = bias_samples
+    num_joints = len(joint_ids_tensor)
+    bias_samples = sample_uniform(
+      torch.tensor(global_bias_range[0], device=env.device),
+      torch.tensor(global_bias_range[1], device=env.device),
+      (len(env_ids), num_joints),
+      env.device,
+    )
 
+    if isinstance(joint_ids, slice):
+      asset.data.encoder_bias[env_ids] = bias_samples
+    else:
+      asset.data.encoder_bias[env_ids[:, None], joint_ids_tensor] = bias_samples
 
 def sync_actuator_delays(
   env: ManagerBasedRlEnv,
