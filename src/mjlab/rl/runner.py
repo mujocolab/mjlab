8780c2db4f86445775733e4e252d8f19bcc23888
Louis Le Lay <louislelay@pal-robotics.com>
2026-02-06 14:13:36 +0100

    makes legacy checkpoint format work

--- src/mjlab/rl/runner.py
+++ src/mjlab/rl/runner.py
@@ -1,7 +1,7 @@
 from rsl_rl.runners import OnPolicyRunner
 
 from mjlab.rl.vecenv_wrapper import RslRlVecEnvWrapper
-
+import torch
 
 class MjlabOnPolicyRunner(OnPolicyRunner):
   """Base runner that persists environment state across checkpoints."""
@@ -14,9 +14,47 @@ class MjlabOnPolicyRunner(OnPolicyRunner):
     super().save(path, infos)
 
   def load(
-    self, path: str, load_cfg: dict | None = None, strict: bool = True, map_location: str | None = None
+    self,
+    path: str,
+    load_cfg: dict | None = None,
+    strict: bool = True,
+    map_location: str | None = None,
   ):
-    infos = super().load(path, load_cfg, strict, map_location)
+    loaded_dict = torch.load(path, map_location=map_location, weights_only=False)
+
+    if "model_state_dict" in loaded_dict:
+      print(f"Detected legacy checkpoint at {path}. Migrating to new format...")
+      model_state_dict = loaded_dict.pop("model_state_dict")
+      actor_state_dict = {}
+      critic_state_dict = {}
+
+      for key, value in model_state_dict.items():
+        # migrate actor keys.
+        if key.startswith("actor."):
+          new_key = key.replace("actor.", "mlp.")
+          actor_state_dict[new_key] = value
+        elif key.startswith("actor_obs_normalizer."):
+          new_key = key.replace("actor_obs_normalizer.", "obs_normalizer.")
+          actor_state_dict[new_key] = value
+        elif key in ["std", "log_std"]:
+          actor_state_dict[key] = value
+
+        # migrate critic keys.
+        if key.startswith("critic."):
+          new_key = key.replace("critic.", "mlp.")
+          critic_state_dict[new_key] = value
+        elif key.startswith("critic_obs_normalizer."):
+          new_key = key.replace("critic_obs_normalizer.", "obs_normalizer.")
+          critic_state_dict[new_key] = value
+
+      loaded_dict["actor_state_dict"] = actor_state_dict
+      loaded_dict["critic_state_dict"] = critic_state_dict
+
+    load_iteration = self.alg.load(loaded_dict, load_cfg, strict)
+    if load_iteration:
+      self.current_learning_iteration = loaded_dict["iter"]
+
+    infos = loaded_dict["infos"]
     if infos and "env_state" in infos:
       self.env.unwrapped.common_step_counter = infos["env_state"]["common_step_counter"]
     return infos
