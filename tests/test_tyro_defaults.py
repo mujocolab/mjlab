3bb39a2dc69f76fba27d1e73664105200558e25c
Robin Deits <rdeits@bostondynamics.com>
2026-02-24 15:16:29 -0500

    Test for regression of #677

---
+++ tests/test_tyro_defaults.py
@@ -0,0 +1,22 @@
+import pytest
+
+import tyro
+from mjlab.scripts.train import TrainConfig
+from mjlab.envs import ManagerBasedRlEnvCfg
+from mjlab.rl import RslRlOnPolicyRunnerCfg
+from mjlab.scene import SceneCfg
+from mjlab.terrains import TerrainEntityCfg
+
+
+def test_tyro_defaults():
+    """
+    Test for regression of https://github.com/mujocolab/mjlab/issues/677 due to incompatibilities between the declared types and default values of the env configs.
+    """
+    cfg = tyro.cli(
+        TrainConfig,
+        default=TrainConfig(env=ManagerBasedRlEnvCfg(decimation=5, scene=SceneCfg(terrain=TerrainEntityCfg(),)), agent=RslRlOnPolicyRunnerCfg()),
+        config=(
+            tyro.conf.AvoidSubcommands,
+            tyro.conf.FlagConversionOff,
+        ), args=[],
+    )
