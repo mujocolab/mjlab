d983a291798a076fefa2ff1370b5cc8fa9f8216d
ManuelActisCassa <manuel.actis.cassa@gmail.com>
2026-03-10 16:34:43 +0100

    Add Adversarial Motion Prior Implementation - Train launches - To be tested

--- src/mjlab/scripts/train.py
+++ src/mjlab/scripts/train.py
@@ -21,6 +21,11 @@ from mjlab.utils.torch import configure_torch_backends
 from mjlab.utils.wandb import add_wandb_tags
 from mjlab.utils.wrappers import VideoRecorder
 
+from mjlab.tasks.AMP.rl import (
+  AmpOnPolicyRunner as AmpOnPolicyRunner,
+  DiscriminatorCfg as DiscriminatorCfg,
+)
+
 
 @dataclass(frozen=True)
 class TrainConfig:
@@ -163,6 +168,12 @@ def run_train(task_id: str, cfg: TrainConfig, log_dir: Path) -> None:
   if rank == 0:
     dump_yaml(log_dir / "params" / "env.yaml", env_cfg)
     dump_yaml(log_dir / "params" / "agent.yaml", agent_cfg)
+
+  if runner_cls is AmpOnPolicyRunner :
+    discriminator_cgf = DiscriminatorCfg()
+    discriminator_cgf.n_obs = 23
+    discriminator_cgf.device = device
+    runner_kwargs["discriminator_cfg"] = discriminator_cgf
 
   runner = runner_cls(env, agent_cfg, str(log_dir), device, **runner_kwargs)
 
